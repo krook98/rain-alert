2c02c389ee103aeedf71e9c0b72aa42537c71550
Jakub Sokalski <kuba@macbook-air-jakub.home>
2022-01-28 17:16:42 +0100

    Checking if it's going to rain

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ API_KEY = 'baff3d7005600d597c7afd426fc8d8e0'
 URL = 'https://api.openweathermap.org/data/2.5/onecall'
 LAT = 52.229675
 LON = 21.012230
+EXCLUDE = "current,minutely,daily"
 
 
-response = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LON}&appid={API_KEY}")
+response = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LON}&exclude={EXCLUDE}"
+                            f"&appid={API_KEY}")
 response.raise_for_status()
 data = response.json()
+weather_slice = data['hourly'][:12]
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]['id']
+    if int(condition_code) < 700:
+        print('Bring an umbrella')
+        break
