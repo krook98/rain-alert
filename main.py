0399a54dcd80db496c18f91a7e23090a0da4af63
Jakub Sokalski <kuba@macbook-air-jakub.home>
2022-01-29 00:44:20 +0100

    Application finished, API keys hidden

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 
-API_KEY = 'baff3d7005600d597c7afd426fc8d8e0'
+API_KEY = os.environ.get('API_KEY')
 URL = 'https://api.openweathermap.org/data/2.5/onecall'
+PHONE_NUMBER = os.environ.get("PHONE_NUMBER")
 LAT = 52.229675
 LON = 21.012230
 EXCLUDE = "current,minutely,daily"
+
+account_sid = os.environ.get("account_sid")
+auth_token = os.environ.get("auth_token")
 
 
 response = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LON}&exclude={EXCLUDE}"
@@ -16,5 +23,20 @@ weather_slice = data['hourly'][:12]
 for hour_data in weather_slice:
     condition_code = hour_data["weather"][0]['id']
     if int(condition_code) < 700:
-        print('Bring an umbrella')
+        
+        proxy_client = TwilioHttpClient()
+        proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+        client = Client(account_sid, auth_token, http_client=proxy_client)
+
+        message = client.messages \
+                        .create(
+                            body="It's going to rain today. Remember to bring an umbrella ☂.",
+                            from_=os.environ.get('NOTIFICATION_NUMBER'),
+                            to=PHONE_NUMBER
+                        )
+
+        print(message.sid)
         break
+
+
